3561fa1c1655dd2844c40e02a81938d90c73adf0
huangjingxin <huangjingxin@meituan.com>
2020-04-03 19:49:59 +0800

    add pageB hot

---
+++ src/page/pageB/app.jsx
@@ -0,0 +1,29 @@
+import React, { lazy, Suspense } from 'react'
+import ReactDom from 'react-dom'
+import Title from './components/a'
+import Button from './components/b'
+import ButtonD from './components/d'
+import Lazy from './components/lazy'
+
+export default class App extends React.PureComponent {
+
+  // state = {
+  //   isShowLazy: false
+  // }
+
+
+  render() {
+    return (
+      <div>
+        <Title />
+        <Button load={() => { }} />
+        <ButtonD />
+        <Lazy
+          componentPromise={import(
+            /* webpackChunkName: "lazyComB" */
+            './components/c'
+          )} />
+      </div>
+    )
+  }
+}
--- src/page/pageB/index.jsx
+++ src/page/pageB/index.jsx
@@ -1,31 +1,11 @@
-import React, { lazy, Suspense } from 'react'
+import React from 'react'
 import ReactDom from 'react-dom'
-import Title from './components/a'
-import Button from './components/b'
-import ButtonD from './components/d'
-import Lazy from './components/lazy'
+import App from './app'
 
-class App extends React.PureComponent {
-
-  // state = {
-  //   isShowLazy: false
-  // }
-
-
-  render() {
-    return (
-      <div>
-        <Title />
-        <Button load={() => { }} />
-        <ButtonD />
-        <Lazy
-          componentPromise={import(
-            /* webpackChunkName: "lazyComB" */
-            './components/c'
-          )} />
-      </div>
-    )
-  }
+if (module.hot) {
+  module.hot.accept('./app.jsx', function () {
+    const App = require("./app.jsx").default;
+    ReactDom.render(<App />, document.getElementById('app'))
+  })
 }
-
 ReactDom.render(<App />, document.getElementById('app'))
